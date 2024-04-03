e7cfd87d538cc0cf0b4ac451902fe47b56d9c0c3
dhansang <164672669+dhansang@users.noreply.github.com>
2024-04-03 23:51:05 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,4 @@
+// Create Web Server with Node.js
+// Run: node comments.js
+// Open browser: http://localhost:3000
+// Test: curl -X POST -H "Content-Type: application/json" -d '{"comment":"This is a comment"}'
