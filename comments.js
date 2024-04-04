47845bf675305e7671d3b148338fbe18ec236ca4
dhansang <164672669+dhansang@users.noreply.github.com>
2024-04-04 00:17:55 +0000

    Changing comments because of build fail

--- comments.js
+++ comments.js
@@ -1,4 +1,3 @@
 // Create Web Server with Node.js
 // Run: node comments.js
-// Open browser: http://localhost:3000
-// Test: curl -X POST -H "Content-Type: application/json" -d '{"comment":"This is a comment"}'
+// Open browser: making changes to comments
