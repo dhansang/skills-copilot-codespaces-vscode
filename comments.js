a268ba2f74787a28ed617b20474d372a862c2f76
dhansang <164672669+dhansang@users.noreply.github.com>
2024-04-03 20:36:38 -0400

    Update comments.js

--- comments.js
+++ comments.js
@@ -1,1 +1,40 @@
-// adding a comment
+// Create Web Server
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+// Load comments from file
+function loadComments() {
+  try {
+    return JSON.parse(fs.readFileSync('comments.json', 'utf8'));
+  } catch (e) {
+    return [];
+  }
+}
+
+// Save comments to file
+function saveComments(comments) {
+  fs.writeFileSync('comments.json', JSON.stringify(comments));
+}
+
+// Get all comments
+app.get('/comments', function (req, res) {
+  res.json(loadComments());
+});
+
+// Add a comment
+app.post('/comments', function (req, res) {
+  var comments = loadComments();
+  comments.push(req.body);
+  saveComments(comments);
+  res.status(201).end();
+});
+
+// Start server
+app.listen(3000, function () {
+  console.log('Server listening on port 3000');
+});
+
